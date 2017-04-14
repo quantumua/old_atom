3d4779257d6b7743de46906eec25def8a87bf39d
vadims <devnull@localhost>
2017-04-14 19:21:21 +0300

    added unit tests for CRMAccount and CRMCustomer

---
+++ core/src/test/java/com/betamedia/qe/af/core/api/tp/entities/response/CRMCustomerTest.java
@@ -0,0 +1,70 @@
+package com.betamedia.qe.af.core.api.tp.entities.response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.testng.AssertJUnit.assertNull;
+import org.testng.annotations.Test;
+import java.math.BigDecimal;
+
+public class CRMCustomerTest {
+
+    private final BigDecimal PENDING_AMOUNT = BigDecimal.valueOf(9999);
+
+    @Test
+    public void testNullAccounts() {
+        CRMCustomer crmCustomer = new CRMCustomer();
+        crmCustomer.setAccounts(null);
+        assertNull(crmCustomer.getBinaryAccount());
+        assertNull(crmCustomer.getFXCFDAccount());
+    }
+
+    @Test
+    public void testNullFieldValuesAccounts() {
+        CRMCustomer crmCustomer = new CRMCustomer();
+        crmCustomer.setAccounts(getNullFieldValuesCRMAccounts());
+        assertNull(crmCustomer.getBinaryAccount());
+        assertNull(crmCustomer.getFXCFDAccount());
+    }
+
+    @Test
+    public void testShouldCheckBinaryAccountWithIncorrectProduct() {
+        CRMCustomer crmCustomer = new CRMCustomer();
+        crmCustomer.setAccounts(getCustomCRMAccounts());
+        assertNull(crmCustomer.getBinaryAccount());
+        assertNull(crmCustomer.getFXCFDAccount());
+    }
+
+    @Test
+    public void testShouldCheckBinaryAccountForCorrectProducts() {
+        CRMAccount scipioCRMAccount = getCRMAccountCustom();
+        scipioCRMAccount.setPlatform("scipio");
+        CRMAccount pandaCRMAccount = getCRMAccountCustom();
+        pandaCRMAccount.setPlatform("panda");
+        CRMCustomer crmCustomer = new CRMCustomer();
+        crmCustomer.setAccounts(new CRMAccount[]{scipioCRMAccount, pandaCRMAccount});
+
+        checkFields(crmCustomer.getBinaryAccount(), scipioCRMAccount);
+        checkFields(crmCustomer.getFXCFDAccount(), pandaCRMAccount);
+    }
+
+    private CRMAccount[] getNullFieldValuesCRMAccounts() {
+        return new CRMAccount[]{getNullFieldValuesCRMAccount()};
+    }
+
+    private CRMAccount[] getCustomCRMAccounts() {
+        return new CRMAccount[]{getCRMAccountCustom()};
+    }
+
+    private void checkFields(CRMAccount actualCRMAccount, CRMAccount expectedCRMAccount) {
+        assertThat(actualCRMAccount, is(samePropertyValuesAs(expectedCRMAccount)));
+    }
+
+    private CRMAccount getNullFieldValuesCRMAccount() {
+        return new CRMAccount(null, null, null, null, null, null, null, null);
+    }
+
+    private CRMAccount getCRMAccountCustom() {
+        return new CRMAccount("id", "currency", "displayId", "accountType", "platform", "product", PENDING_AMOUNT, "externalId");
+    }
+}
