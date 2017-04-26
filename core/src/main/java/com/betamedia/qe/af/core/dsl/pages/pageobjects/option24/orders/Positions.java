896092fbafce2459e5ce7155114d6855251a9cc8
Leonid Artemyev <leonid.a@scipiosoft.com>
2017-04-26 19:19:31 +0300

    Compile error fix

--- core/src/main/java/com/betamedia/qe/af/core/dsl/pages/pageobjects/option24/orders/Positions.java
+++ core/src/main/java/com/betamedia/qe/af/core/dsl/pages/pageobjects/option24/orders/Positions.java
@@ -2,6 +2,8 @@ package com.betamedia.qe.af.core.dsl.pages.pageobjects.option24.orders;
 
 import java.util.List;
 
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by mbelyaev on 3/27/17.
  */
