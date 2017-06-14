a8717ad398ca4a9bc0d87eb05226631d42fe5264
maksym.b <devnull@localhost>
2017-06-14 14:50:18 +0300

    new automation operations

---
+++ core/src/main/java/com/betamedia/atom/core/dsl/operations/impl/newautomation/NewAutomationEnvOnBoardingOperationsImpl.java
@@ -0,0 +1,13 @@
+package com.betamedia.atom.core.dsl.operations.impl.newautomation;
+
+import com.betamedia.atom.core.dsl.operations.impl.AbstractOnBoardingOperations;
+import com.betamedia.atom.core.environment.tp.NewAutomationEnvironment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mbelyaev
+ * @since 5/19/17
+ */
+@Component
+public class NewAutomationEnvOnBoardingOperationsImpl extends AbstractOnBoardingOperations<NewAutomationEnvironment> implements NewAutomationEnvironment {
+}
