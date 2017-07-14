3422544a4d06ed53357b4a134c0a99f7852cf666
vadims <devnull@localhost>
2017-07-14 12:27:53 +0300

    Added java docs for the methods.

--- testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/end2end/crm/createnewcustomers/CreateNewCustomersCrm.java
+++ testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/end2end/crm/createnewcustomers/CreateNewCustomersCrm.java
@@ -1,16 +1,8 @@
 package com.betamedia.atom.testslibrary.option24.end2end.crm.createnewcustomers;
 
-import com.betamedia.atom.core.api.tp.entities.namingstrategies.customer.CRMMobileAPINamingStrategy;
-import com.betamedia.atom.core.api.tp.entities.namingstrategies.customer.WidgetsNamingStrategy;
-import com.betamedia.atom.core.api.tp.entities.request.CustomerRO;
 import com.betamedia.atom.core.api.tp.entities.response.CRMCustomer;
-import com.betamedia.atom.testslibrary.option24.end2end.bmw.AbstractOnboardingUserExperienceTest;
-import com.betamedia.atom.testslibrary.option24.end2end.crm.experienceLevels.AbstractUserExperienceTest;
 import com.betamedia.atom.testslibrary.option24.end2end.crm.experienceLevels.CrmUserExperienceTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static org.testng.AssertJUnit.*;
 
 /**
  * Created by vsnigur on 7/3/17.
@@ -19,6 +11,10 @@ public class CreateNewCustomersCrm extends CrmUserExperienceTest {
 
     private static int WIDGET_SOURCE_ID = 206440002;
 
+    /**
+     * - create new customer using widget web site;
+     * - verify customer creation source;
+     */
     @Test(description = "CTW-2544: Web widget")
     public void checkTrafficSourceAndMarketingDataValuesInDataBase() {
         pages().crmNavigation().register();
--- testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/createnewcustomers/CreateNewCustomers.java
+++ testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/createnewcustomers/CreateNewCustomers.java
@@ -40,6 +40,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
     private static final int ZERO_VALUE = 0;
     private static final int WEB_SOURCE_ID = 206440004;
 
+    /**
+     * - verify that sign up button opens registration dialog
+     * - verify registration dialog caption
+     */
     @Test(description = "CTW-5079:verify sign up button gives you the open account pop up")
     public void verifySignUpButtonRedirectToOnboardingOpenAccount() {
         pages().topNavigationPage().signUp();
@@ -47,6 +51,13 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertEquals("Apply for an account with 24option", pages().registrationDialog().dialogCaption());
     }
 
+    /**
+     * Verify registration page elements
+     * - logo;
+     * - login button;
+     * - risk message;
+     * - live chat control;
+     */
     @Test(description = "CTW-5089:main page elements verification")
     public void applyForAnAccountElementsVerification() {
         pages().topNavigationPage().signUp();
@@ -58,6 +69,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertTrue("Live chat link does not appear.", pages().registrationDialog().chatLinkDisplayed());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate first name field;
+     */
     @Test(description = "CTW-5375:first name field validations")
     public void applyForAnAccountFirstNameFieldValidations() {
         CustomerRO customerRO = CustomerRO.builder(
@@ -73,6 +88,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertEquals(FAIL_NAME_NOTIFICATION, pages().registrationDialog().getErrorMessageNotification());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate last name field;
+     */
     @Test(description = "CTW-5208:last name field validations")
     public void applyForAnAccountLastNameFieldValidations() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get())
@@ -88,6 +107,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertEquals(FAIL_NAME_NOTIFICATION, pages().registrationDialog().getErrorMessageNotification());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate email name field;
+     */
     @Test(description = "CTW-5209:email field validations")
     public void emailFieldValidations() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get())
@@ -107,6 +130,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertEquals(GREEN_RGB_STYLE, pages().registrationDialog().getBorderColorForEmail());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate prefix field field;
+     */
     @Test(description = "CTW-5210:prefix field validations")
     public void prefixFieldValidations() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -122,6 +149,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertEquals(RED_RGB_STYLE, pages().registrationDialog().getBorderForCountryField());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate phone field;
+     */
     @Test(description = "CTW-5211:phone number field validations")
     public void phoneFieldValidations() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get())
@@ -134,6 +165,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertEquals("Enter between 6 to 15 numbers", pages().registrationDialog().getErrorMessageNotification());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate country search functionality for field;
+     */
     @Test(description = "CTW-5260:country dropdown field search engine")
     public void countryDropdownFieldSearchEngine() {
         pages().topNavigationPage().signUp();
@@ -144,6 +179,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 IRELAND_COUNTRY, pages().registrationDialog().countrySearch("I", IRELAND_COUNTRY));
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate country field field;
+     */
     @Test(description = "CTW-5261:country dropdown validations")
     public void countryDropdownValidations() {
         pages().topNavigationPage().signUp();
@@ -157,6 +196,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 pages().registrationDialog().getErrorMessageNotification());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate currency field;
+     */
     @Test(description = "CTW-5262:currency dropdown field validation")
     public void currencyDropdownFieldValidation() {
         pages().topNavigationPage().signUp();
@@ -166,6 +209,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 pages().registrationDialog().availableCurrencies().isEmpty());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate password field;
+     */
     @Test(description = "CTW-5264:password fields validations")
     public void passwordFieldsValidation() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get())
@@ -202,6 +249,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 pages().startTradeDialog().exists());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate agreement checkbox for message notification;
+     */
     @Test(description = "CTW-5265:checkbox validation")
     public void agreementCheckboxValidation() {
         pages().topNavigationPage().signUp();
@@ -213,6 +264,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 pages().registrationDialog().agreementStatus());
     }
 
+    /**
+     * - generate user account using builder;
+     * - validate submit button functionality;
+     */
     @Test(description = "CTW-5359:submit button")
     public void submitButtonValidation() {
         pages().topNavigationPage().signUp();
@@ -222,6 +277,11 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         assertTrue(pages().startTradeDialog().exists());
     }
 
+    /**
+     * - generate 2 user account using mobile API registration;
+     * - add same credit cards for the users using UI;
+     * - verify users connection in the DB;
+     */
     @Test(description = "CTW-17742:Same Credit Card")
     public void validateConnectionInDBForDifferentCustomersButSameCreditCard() {
         CustomerRO customerROFirst = registerCustomer();
@@ -234,6 +294,11 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 "Same Credit Card");
     }
 
+    /**
+     * - generate 2 user account using builder;
+     * - create 2 users using UI with the same phone;
+     * - verify users connection in the DB;
+     */
     @Test(description = "CTW-17744:Same Phone number1")
     public void validateConnectionInDBForDifferentCustomersButSamePhone() {
         CustomerRO customerROFirst = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -253,6 +318,11 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 "Same Phone");
     }
 
+    /**
+     * - generate 2 user using mobile API;
+     * - create 2 users with the same first and last names;
+     * - verify users are not connected in the DB;
+     */
     @Test(description = "CTW-17745:Same First and last name")
     public void validateNoConnectionsInDBForDifferentCustomersWithSameFirstAndLastNames() {
         CustomerRO customerROFirst = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -269,6 +339,11 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 customerROSecond.getEmail());
     }
 
+    /**
+     * - generate 2 user using builder;
+     * - try create 2 users with the same email;
+     * - verify that impossible to create 2 users with the same email;
+     */
     @Test(description = "CTW-17743:Same Email")
     public void validateNoConnectionsInDBForDifferentCustomersWithSameEmail() {
         CustomerRO customerROFirst = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -285,6 +360,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 pages().registrationDialog().getErrorMessageNotification());
     }
 
+    /**
+     * - create 2 user using UI from the same workstation;
+     * - verify users are not connected in the DB;
+     */
     @Test(description = "CTW-17746:Same Machine ID -negative")
     public void validateNoConnectionsInDBForDifferentCustomersWithSameMachineID() {
         CustomerRO customerROFirst = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -298,6 +377,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 customerROSecond.getEmail());
     }
 
+    /**
+     * - create user using UI;
+     * - verify bulkEmail field value for user, it is not 0;
+     */
     @Test(description = "CTW-7884:Bulk emails indicator Allow=No by default")
     public void validateBulkEmailHasNoneZeroForNewCreatedCustomer() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -305,6 +388,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         operations().onBoardingOperations().assertBulkEmailHasNotValue(customerRO.getEmail(), ZERO_VALUE);
     }
 
+    /**
+     * - create user using UI;
+     * - verify phone calls field value for user, it is not 0;
+     */
     @Test(description = "CTW-9009:Phone calls indicator = Allow by default")
     public void validatePhoneCallsForNewCreatedCustomer() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -312,6 +399,10 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         operations().onBoardingOperations().assertDoNotPhoneHasNotValue(customerRO.getEmail(), String.valueOf(ZERO_VALUE));
     }
 
+    /**
+     * - create user using IU;
+     * - verify customer creation source;
+     */
     @Test(description = "CTW-2544: Web page")
     public void validateCustomerCreationSourceId() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
@@ -319,12 +410,19 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
         operations().onBoardingOperations().assertTrafficSource(customerRO.getEmail(), WEB_SOURCE_ID);
     }
 
+    /**
+     * create user using customerRO build object
+     * @param customerRO - customerRO build object;
+     */
     private void createCustomer(CustomerRO customerRO) {
         pages().topNavigationPage().signUp();
         pages().registrationDialog().register(customerRO);
         pages().welcomePage().isStartBtnDisplayed();
     }
 
+    /**
+     * pass questionnaire for newly created user;
+     */
     private void passQuestionnaire() {
         pages().fnsPersonalInformation().submitOnWizard(PersonalInformation.builder()
                 .withEmploymentStatus(Questions.EmploymentStatus.SALARIED_EMPLOYEE.get())
@@ -365,11 +463,12 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
                 .build());
     }
 
+    /**
+     * register new customer using UI;
+     * @return - customerRO object with fields for created user.
+     */
     private CustomerRO registerCustomer() {
         CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
-        operations().customerOperations().updateCustomersOnboardingConditions(
-                operations().customerOperations().register(customerRO),
-                createConditionsToShowOnlyDepositPage());
         pages().topNavigationPage().logIn();
         pages().loginPage().login(customerRO.getEmail(),customerRO.getPassword());
         pages().welcomeBackMessage().exists();
@@ -384,54 +483,5 @@ public class CreateNewCustomers extends AbstractOnboardingUserExperienceTest {
             Reporter.log("Upload documents dialog did not appear");
         }
         return customerRO;
-    }
-
-    private OnboardingWizardConditions createConditionsToShowOnlyDepositPage() {
-        return new OnboardingWizardConditions(true, true, true, true,
-                OnboardingWizardConditions.ExperienceLevel.HIGH_EXPERIENCE, false,
-                OnboardingWizardConditions.AccountType.REAL,
-                OnboardingWizardConditions.DocumentVerificationStatus.VERIFIED,
-                OnboardingWizardConditions.DocumentVerificationStatus.VERIFIED, true,
-                true, OnboardingWizardConditions.DocumentVerificationStatus.VERIFIED);
-    }
-
-    private CustomerRO createCustomerROUser() {
-        CustomerRO customerRO = CustomerRO.builder(CRMMobileAPINamingStrategy.get()).build();
-        pages().browser().deleteAllCookies();
-        pages().browser().refreshPage();
-        pages().topNavigationPage().signUp();
-        pages().registrationDialog().register(customerRO);
-        pages().startTradeDialog().startTrade();
-        pages().accountAdditionalDetails().update(AccountAdditionalDetails.builder().build());
-        passTradingQuestionnaire();
-        pages().creditCardDeposit().submit(CreditCardDeposit.builder().build());
-        return customerRO;
-    }
-
-    private void passTradingQuestionnaire() {
-        pages().fnsTradingExperience().submitOnWizard(TradingExperienceInfo.builder()
-                .withSharesExperience(Questions.SharesExperience.FREQUENTLY.get())
-                .withBinaryExperience(Questions.BinaryExperience.FREQUENTLY.get())
-                .withAverageYearlyBinaryVolume(Questions.AverageYearlyBinaryVolume.VOLUME_ABOVE_10K.get())
-                .withForExExperience(Questions.ForExExperience.FREQUENTLY.get())
-                .withAverageYearlyForExVolume(Questions.AverageYearlyForExVolume.VOLUME_ABOVE_500K.get())
-                .withCommonLeverage(Questions.CommonLeverage.LEVERAGE_ABOVE_1TO500.get())
-                .withFinancialWorkExperience(Questions.FinancialWorkExperience.SEMINARS.get())
-                .withCfdBinaryKnowledge(Questions.CfdBinaryKnowledge.SPECULATIVE.get())
-                .withMainFactorKnowledge(Questions.MainFactorKnowledge.INTEREST_RATES.get())
-                .withHowToCloseKnowledge(Questions.HowToCloseKnowledge.ONLY_PLATFORM.get())
-                .withCfdLeverageKnowledge(Questions.CfdLeverageKnowledge.MAGNIFIES.get())
-                .withStopLossKnowledge(Questions.StopLossKnowledge.MINIMIZE.get())
-                .withRequiredMarginKnowledge(Questions.RequiredMarginKnowledge.MARGIN_1K.get())
-                .withMarginLevelDropKnowledge(Questions.MarginLevelDropKnowledge.MARGIN_CALL.get())
-                .withAutomaticStopKnowledge(Questions.AutomaticStopKnowledge.EQUITY_FALLS.get())
-                .withLossOn1to50Knowledge(Questions.LossOn1to50Knowledge.A1_800.get())
-                .withLossOn1to200Knowledge(Questions.LossOn1to200Knowledge.A1_1800.get())
-                .withBinaryInvestProfitKnowledge(Questions.BinaryInvestProfitKnowledge.PROFIT_75.get())
-                .withBinaryInvestLossKnowledge(Questions.BinaryInvestLossKnowledge.LOSS_100.get())
-                .withBinaryProbabilityKnowledge(Questions.BinaryProbabilityKnowledge.MONEY_25.get())
-                .build()
-        );
-        pages().confirmAnswers().next();
     }
 }
