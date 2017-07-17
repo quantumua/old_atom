c2d2d7e386ef198b60caa5c4ae27110a5e20d5d5
Leonid Artemiev <leonid.a@scipiosoft.com>
2017-07-17 18:55:36 +0300

    Changed inheritance from WEBEndToEndTest to TPEndToEndTest

--- testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/ctw/PersonalDetailsSlideLocalisationTest.java
+++ testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/ctw/PersonalDetailsSlideLocalisationTest.java
@@ -4,14 +4,16 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.betamedia.atom.core.api.crm.form.entities.AccountAdditionalDetailsData;
-import com.betamedia.atom.core.testingtype.web.WEBEndToEndTest;
+import com.betamedia.atom.core.api.tp.entities.namingstrategies.customer.WebSiteNamingStrategy;
+import com.betamedia.atom.core.api.tp.entities.request.CustomerRO;
+import com.betamedia.atom.core.testingtype.tp.TPEndToEndTest;
 
 /**
  * @author Leonid Artemiev
  * @since 7/13/17
  */
 
-public class PersonalDetailsSlideLocalisationTest extends WEBEndToEndTest {//AbstractOnboardingUserExperienceTest{
+public class PersonalDetailsSlideLocalisationTest extends TPEndToEndTest {
 	/*
 	 *[testlink]  CTW-5680:Verify the slide is translated to all languages
 	 */
@@ -39,12 +41,13 @@ public class PersonalDetailsSlideLocalisationTest extends WEBEndToEndTest {//Abs
     @Test(description = "CTW-5682:Verify the slide turns RTL on AR")
     public void  verifyTheSlideTurnsRTLOnAR(String countrycode) {
         pages().topNavigationPage().signUp();
-        pages().registrationPage().register(countrycode);
-        pages().welcomepage().isStartBtnDisplayed();
-        pages().welcomepage().start();
+        CustomerRO customer = CustomerRO.builder(WebSiteNamingStrategy.get()).setCountryCode(countrycode).build();
+        pages().registrationDialog().register(customer);
+        pages().welcomePage().isStartBtnDisplayed();
+        pages().welcomePage().start();
         pages().topNavigationPage().selectLanguage("AR");
-        pages().welcomeBackMessage().continueQuestionnaire();                
-        pages().accountAdditionalDetailsPage().exists();
-        pages().accountAdditionalDetailsPage().verifyTextDirectionElements("RTL");        
+        pages().welcomeBackMessage().continueQuestionnaire();
+        pages().accountAdditionalDetails().exists();
+        pages().accountAdditionalDetails().verifyTextDirectionElements("RTL");
     }
 }
