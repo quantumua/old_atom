bb3b2b97d2c89a6a8ddb399b9f76b7dc4c26ed22
maksym.b <devnull@localhost>
2017-07-18 16:20:00 +0300

    database repositories toggle (grafted from cf3c3e82dd6635b27bd41d4f2300652e35a5014e)

--- core/src/main/java/com/betamedia/atom/core/configuration/environment/persistence/NewAutomationPersistenceConfig.java
+++ core/src/main/java/com/betamedia/atom/core/configuration/environment/persistence/NewAutomationPersistenceConfig.java
@@ -7,6 +7,7 @@ import com.betamedia.atom.core.persistence.entities.ContactExtension;
 import com.betamedia.atom.core.persistence.repositories.impl.newautomation.NewAutomationEnvTrackingInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
+
+import static com.betamedia.atom.core.configuration.environment.persistence.StubConfig.DB_ENABLED_PROPERTY;
 
 /**
  * New Automation Environment-specific repository configuration class.
@@ -32,6 +35,7 @@ import javax.sql.DataSource;
         },
         entityManagerFactoryRef = "newAutomationEntityManagerFactory"
 )
+@ConditionalOnProperty(name = DB_ENABLED_PROPERTY, matchIfMissing = true)
 @ConditionalOnBean(NewAutomationEnvironmentConfig.class)
 public class NewAutomationPersistenceConfig {
     @Autowired
