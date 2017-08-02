b0fa5152bb549eb3757916d4b3f3676690ed676a
Leonid Artemyev <leonid.a@scipiosoft.com>
2017-08-02 21:57:29 +0300

    1. Removed not deprecated class files

--- core/src/main/java/com/betamedia/atom/core/dsl/pages/pageobjects/option24/onboarding/DocumentUploadForm.java
+++
@@ -1,8 +0,0 @@
-package com.betamedia.atom.core.dsl.pages.pageobjects.option24.onboarding;
-
-/**
- * Created by mbelyaev on 5/24/17.
- */
-public interface DocumentUploadForm {
-    void uploadIdCard();
-}
