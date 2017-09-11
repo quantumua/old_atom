8711883cd53f0d932b4f0ace0b4d94b04773626d
Leonid Artemyev <leonid.a@scipiosoft.com>
2017-09-11 16:53:16 +0300

    1. Moved test cases to new DepositSlideFunctionalityTest.java class. 2. Added proper depositSlideFunctionality.xml file.

---
+++ testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/depositSlide/DepositSlideFunctionalityTest.java
@@ -0,0 +1,138 @@
+package com.betamedia.atom.testslibrary.option24.web.depositSlide;
+
+import com.betamedia.atom.core.api.crm.form.entities.CreditCardDeposit;
+import com.betamedia.atom.core.testlink.annotations.TestLinkProperties;
+import org.testng.annotations.*;
+
+/**
+ * Created by lartemyev on 9/7/17.
+ */
+public class DepositSlideFunctionalityTest extends  DepositSlideSanityTest {
+
+    final private static String AMOUNT_MAX_PLUS_1_DIGITS = "12345678911";
+    final private static String AMOUNT_THE_FIELD_NUST_NOT_EXCEED_10_CHARS_MESSAGE = "The field length must not exceed 10 characters";
+    final private static String FAILED_AMOUNT_NON_DIGITS_STRING = "test";
+    final private static String AMOUNT_DIGITS_ONLY_ALLOWED_MESSAGE = "Please Enter Only Numbers";
+    final private static String FAILED_DEPOSIT_WITH_CHAR = "50cd";
+    final private static String FAILED_CC_WITH_CHAR = "CREDITCARD";
+    final private static String FAILED_CVV_LONG = "12345";
+    final private static String CVV_INVALID_CODE_MESSAGE = "Invalid code, please enter the last 3 digits written on the back of your card";
+    final private static String CREDIT_CARD_REGULAR_EXPRESSION  = "\\d{4}-\\d{4}-\\d{4}-\\d{4}";
+
+    /*
+     *[TestLink] CTW-19425:Amount char limitation
+     */
+    @Test(description = "CTW-19425:Amount char limitation")
+    @TestLinkProperties(displayId = "CTW-19425")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void amountCharLimitation(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix) {
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
+                .withDepositAmount(AMOUNT_MAX_PLUS_1_DIGITS)
+                .build());
+        softAssert().assertEquals(
+                pages().creditCardDepositDialog().getErrorMessageHint(),
+                AMOUNT_THE_FIELD_NUST_NOT_EXCEED_10_CHARS_MESSAGE,
+                "Verification for '" + AMOUNT_THE_FIELD_NUST_NOT_EXCEED_10_CHARS_MESSAGE + "' tooltip message");
+    }
+
+    /*
+     *[TestLink] CTW-19429:Digits insertion to amount fiel
+     */
+    @Test(description = "CTW-19429:Digits insertion to amount field")
+    @TestLinkProperties(displayId = "CTW-19429")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void digitsInsertionToAmountField(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix) {
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
+                .withDepositAmount(FAILED_AMOUNT_NON_DIGITS_STRING)
+                .build());
+        softAssert().assertEquals(
+                pages().creditCardDepositDialog().getErrorMessageHint(),
+                AMOUNT_DIGITS_ONLY_ALLOWED_MESSAGE,
+                "Verification for '" + AMOUNT_DIGITS_ONLY_ALLOWED_MESSAGE + "' tooltip message");
+    }
+
+    /*
+     * [TestLink] CTW-19430:CC number input type
+     */
+    @Test(description = "CTW-19430:CC number input type")
+    @TestLinkProperties(displayId = "CTW-19430")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void ccNumberInputType(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
+                .withCreditCardNumber(FAILED_CC_WITH_CHAR)
+                .build());
+        softAssert().assertEquals(
+                pages().creditCardDepositDialog().getCreditCardNumber(),
+                "",
+                "Verification that user can`t input in CC field symbol different than numbers");
+    }
+
+    /*
+     * [TestLink] CTW-19432:CC field seperators
+     */
+    @Test(description = "CTW-19432:CC field seperators")
+    @TestLinkProperties(displayId = "CTW-19432")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void ccFieldSeparators(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
+                .withDepositAmount(FAILED_DEPOSIT_WITH_CHAR)
+                .build());
+        softAssert().assertTrue(
+                pages().creditCardDepositDialog().getCreditCardNumber().matches(CREDIT_CARD_REGULAR_EXPRESSION),
+                "Verification for CC field seperators");
+    }
+
+
+    /*
+     * [TestLink] CTW-19434:Month Drop down
+     */
+    @Test(description = "CTW-19434:Month Drop down")
+    @TestLinkProperties(displayId = "CTW-19434")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void monthDropDown(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        softAssert().assertEquals(
+                pages().creditCardDepositDialog().getExpiryDateMonthList().size(),13,
+                "Verification expiry month drop down contains 1-12 month");
+        pages().creditCardDepositDialog().selectExpiryDateMonth();
+        softAssert().assertEquals(
+                pages().creditCardDepositDialog().getExpiryDateMonthSelectedItem(),3,
+                "Verification expiry month drop down is clicable");
+    }
+
+    /*
+     * [TestLink] CTW-19435:Year Drop down
+     */
+    @Test(description = "CTW-19435:Year Drop down")
+    @TestLinkProperties(displayId = "CTW-19435")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void yearDropDown(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        softAssert().assertEquals(pages().creditCardDepositDialog().getExpiryDateYearList().size(),16,
+                "Verification expiry years drop down contains data");
+        pages().creditCardDepositDialog().selectExpiryDateYear();
+        softAssert().assertEquals(pages().creditCardDepositDialog().getExpiryDateYearSelectedItem(),2019,
+                "Verification expiry year drop down is clicable");
+    }
+
+    /*
+     * [TestLink] CTW-19436:CVV digits
+     */
+    @Test(description = "CTW-19436:CVV digits")
+    @TestLinkProperties(displayId = "CTW-19436")
+    @Parameters({"countrycode", "phonecountryprefix"})
+    public void cvvDigits(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
+        invokeDepositSlide(countrycode, phonecountryprefix);
+        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
+                .withCVV2(FAILED_CVV_LONG)
+                .build());
+        softAssert().assertEquals(
+                pages().creditCardDepositDialog().getErrorMessageHint(),
+                CVV_INVALID_CODE_MESSAGE,
+                "Verification for long CVV code");
+    }
+}
--- testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/depositSlide/DepositSlideSanityTest.java
+++ testslibrary/src/main/java/com/betamedia/atom/testslibrary/option24/web/depositSlide/DepositSlideSanityTest.java
@@ -18,13 +18,7 @@ import org.testng.annotations.*;
 public class DepositSlideSanityTest extends AbstractWebNavigationTest {
 
     final private static String DEPOSIT = "500";
-    final private static String FAILED_DEPOSIT = "100000000000";
-    final private static String FAILED_DEPOSIT_WITH_CHAR = "50cd";
-    final private static String FAILED_CC_WITH_CHAR = "CREDITCARD";
     final private static String FAILED_CREDIT_CARD_VISA = "4000027891380962";
-    final private static String FAILED_CVV_LONG = "12345";
-    final private static String CREDIT_CARD_REGULAR_EXPRESSION  = "\\d{4}-\\d{4}-\\d{4}-\\d{4}";
-
 
     /*
      *[TestLink]CTW-19424:Deposit E2E EU
@@ -65,113 +59,6 @@ public class DepositSlideSanityTest extends AbstractWebNavigationTest {
     public void depositE2EINTfailure(@Optional("Zambia") String countrycode, @Optional("+260") String phonecountryprefix) {
         depositE2EFailureTest(countrycode, phonecountryprefix);
     }
-
-    /*
-     *[TestLink] CTW-19425:Amount char limitation
-     */
-    @Test(description = "CTW-19425:Amount char limitation")
-    @TestLinkProperties(displayId = "CTW-19425")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void amountCharLimitation(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
-                .withDepositAmount(FAILED_DEPOSIT)
-                .build());
-        softAssert().assertEquals(pages().creditCardDepositDialog().getErrorMessageHint(),"The field length must not exceed 10 characters",
-                "Verification for tooltip loads explaining that amount cannot be bigger than 10 chars");
-    }
-
-    /*
-     *[TestLink] CTW-19429:Digits insertion to amount field
-     */
-    @Test(description = "CTW-19429:Digits insertion to amount field")
-    @TestLinkProperties(displayId = "CTW-19429")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void digitsInsertionToAmountField(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
-                .withDepositAmount(FAILED_DEPOSIT_WITH_CHAR)
-                .build());
-        softAssert().assertEquals(pages().creditCardDepositDialog().getErrorMessageHint(),"Please Enter Only Numbers",
-                "Verification for tooltip loads explaining the possibility insert only digits");
-    }
-
-    /*
-     * [TestLink] CTW-19430:CC number input type
-     */
-    @Test(description = "CTW-19430:CC number input type")
-    @TestLinkProperties(displayId = "CTW-19430")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void ccNumberInputType(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
-                .withCreditCardNumber(FAILED_CC_WITH_CHAR)
-                .build());
-        softAssert().assertEquals(pages().creditCardDepositDialog().getCreditCardNumber(),"",
-                "Verification that user can`t input in CC field symbol different than numbers");
-    }
-
-    /*
-     * [TestLink] CTW-19432:CC field seperators
-     */
-    @Test(description = "CTW-19432:CC field seperators")
-    @TestLinkProperties(displayId = "CTW-19432")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void ccFieldSeparators(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
-                .withDepositAmount(FAILED_DEPOSIT_WITH_CHAR)
-                .build());
-        softAssert().assertTrue(pages().creditCardDepositDialog().getCreditCardNumber().matches(CREDIT_CARD_REGULAR_EXPRESSION),
-                "Verification for CC field seperators");
-    }
-
-
-    /*
-     * [TestLink] CTW-19434:Month Drop down
-     */
-    @Test(description = "CTW-19434:Month Drop down")
-    @TestLinkProperties(displayId = "CTW-19434")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void monthDropDown(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        softAssert().assertEquals(pages().creditCardDepositDialog().getExpiryDateMonthList().size(),13,
-                "Verification expiry month drop down contains 1-12 month");
-        pages().creditCardDepositDialog().selectExpiryDateMonth();
-        softAssert().assertEquals(pages().creditCardDepositDialog().getExpiryDateMonthSelectedItem(),3,
-                "Verification expiry month drop down is clicable");
-    }
-
-    /*
-     * [TestLink] CTW-19435:Year Drop down
-     */
-    @Test(description = "CTW-19435:Year Drop down")
-    @TestLinkProperties(displayId = "CTW-19435")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void yearDropDown(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        softAssert().assertEquals(pages().creditCardDepositDialog().getExpiryDateYearList().size(),16,
-                "Verification expiry years drop down contains data");
-        pages().creditCardDepositDialog().selectExpiryDateYear();
-        softAssert().assertEquals(pages().creditCardDepositDialog().getExpiryDateYearSelectedItem(),2019,
-                "Verification expiry year drop down is clicable");
-    }
-
-    /*
-     * [TestLink] CTW-19436:CVV digits
-     */
-    @Test(description = "CTW-19436:CVV digits")
-    @TestLinkProperties(displayId = "CTW-19436")
-    @Parameters({"countrycode", "phonecountryprefix"})
-    public void cvvDigits(@Optional("Germany") String countrycode, @Optional("+49") String phonecountryprefix){
-        invokeDepositSlide(countrycode, phonecountryprefix);
-        pages().creditCardDepositDialog().submit(CreditCardDeposit.builder()
-                .withCVV2(FAILED_CVV_LONG)
-                .build());
-        softAssert().assertEquals(pages().creditCardDepositDialog().getErrorMessageHint(),"Invalid code, please enter the last 3 digits written on the back of your card",
-                "Verification for long CVV code");
-    }
-
 
     // private test methods
 
